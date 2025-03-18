d933e66d95bc58917a5db813d664e4df497b63c9
sakiadachi <sakiadachi.88@gmail.com>
2025-03-18 19:47:09 +0900

    feat: eslint, prettier導入

--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -1,9 +1,7 @@
 <template>
   <main>
     <template v-if="isLoading">
-      <div class="loading">
-        データを読み込み中...
-      </div>
+      <div class="loading">データを読み込み中...</div>
     </template>
 
     <template v-else>
@@ -18,10 +16,10 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="inventory in inventories">
-            <td><img :src="inventory.item_image.url" alt=""></td>
+          <tr v-for="inventory in inventories" :key="inventory.id">
+            <td><img :src="inventory.item_image.url" alt="" /></td>
             <td>{{ inventory.title }}</td>
-            <td>{{ inventory.quantity }}  {{ inventory.unit }}</td>
+            <td>{{ inventory.quantity }} {{ inventory.unit }}</td>
             <td>{{ inventory.category }}</td>
             <td>
               <router-link :to="`/inventory/${inventory.id}`">詳細</router-link>
@@ -35,17 +33,17 @@
 
 <script setup lang="ts">
 import axios from 'axios'
-import { ref, onMounted } from "vue";
+import { ref, onMounted } from 'vue'
 
 interface InventoryItem {
-  id: number;
-  title: string;
-  quantity: number;
-  unit: string;
-  category: string;
+  id: number
+  title: string
+  quantity: number
+  unit: string
+  category: string
   item_image: {
-    url: string;
-  };
+    url: string
+  }
 }
 
 const inventories = ref<InventoryItem[]>([])
@@ -56,8 +54,8 @@ const getInventory = async () => {
   try {
     const response = await axios.get('https://web.zaico.co.jp/api/v1/inventories', {
       headers: {
-        Authorization: 'Bearer ' + `${import.meta.env.VITE_API_TOKEN}`
-      }
+        Authorization: 'Bearer ' + `${import.meta.env.VITE_API_TOKEN}`,
+      },
     })
     inventories.value = response.data
     console.log(response.data)
