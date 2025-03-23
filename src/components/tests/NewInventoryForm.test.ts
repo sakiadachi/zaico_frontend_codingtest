bfaa0080bbbeff4586a61741f0eb972c26f0fd5e
sakiadachi <sakiadachi.88@gmail.com>
2025-03-23 21:35:10 +0900

    feat: コンポーネントテスト追加

---
+++ src/components/tests/NewInventoryForm.test.ts
@@ -0,0 +1,37 @@
+// ダミーテスト
+import { describe, it, expect } from 'vitest'
+
+import { mount } from '@vue/test-utils'
+import NewInventoryForm from '../templates/inventory-add/NewInventoryForm.vue'
+
+describe('NewInventoryForm', () => {
+  it('ローディング中の場合、フォームのサブミットイベントをemitしない', () => {
+    const wrapper = mount(NewInventoryForm, {
+      props: {
+        isLoading: true,
+      },
+    })
+    const instance = wrapper.vm
+    // TODO: anyを使わない コンポーネントのmothodの型がうまく取得できていない
+    const onSubmit = (instance as any).onSubmit
+
+    onSubmit()
+
+    expect(wrapper.emitted()).not.toHaveProperty('onSubmit')
+  })
+  it('ローディング中ではない場合、フォームのサブミットイベントをemitする', () => {
+    const wrapper = mount(NewInventoryForm, {
+      props: {
+        isLoading: false,
+      },
+    })
+
+    const instance = wrapper.vm
+    // TODO: anyを使わない コンポーネントのmothodの型がうまく取得できていない
+    const onSubmit = (instance as any).onSubmit
+
+    onSubmit()
+
+    expect(wrapper.emitted()).toHaveProperty('onSubmit')
+  })
+})
